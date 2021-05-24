333897a442d7c836788e03a231bb8fbc224d842d
Bruno Silveira dos Anjos Lima <82234250+BrunoDuzanjo@users.noreply.github.com>
2021-05-24 11:57:44 -0300

    Delete movimentoFolha.py

--- movimentoFolha.py
+++
@@ -1,6 +0,0 @@
-class MovimentoFolha:
-    def __init__(self, codigocolaborador, descricao, valor, tipomovimento):
-        self.codigoColaborador = codigocolaborador
-        self.descricao = descricao
-        self.valor = valor
-        self.tipoMovimento = tipomovimento
